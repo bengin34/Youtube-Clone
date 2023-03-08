18f2803d2294094057fc42b1023a9f4273768fc5
bengin34 <becaglar3434@gmail.com>
2023-03-08 17:40:56 +0300

    detailpage

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import { Box } from "@mui/material";
+import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { fetchFromApi } from "../utils/fetchFromApi";
+import { Videos, ChannelCard } from "./";
 
 const ChannelDetail = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
+  const { id } = useParams();
+  console.log(channelDetail, videos);
+  useEffect(() => {
+    fetchFromApi(`channels?part=snippet&id=${id}`).then((data) =>
+      setChannelDetail(data?.items[0])
+    );
 
-export default ChannelDetail
+    fetchFromApi(`search?channelId=${id}&part=snippet&order=date`).then(
+      (data) => setVideos(data?.items)
+    );
+  }, [id]);
+  return (
+    <Box minHeight="95vh">
+    <Box>
+      <div
+        style={{
+          background: "rgb(99,101,53)",
+          background:
+            "linear-gradient(17deg, rgba(99,101,53,1) 18%, rgba(36,0,0,1) 51%, rgba(212,0,255,1) 83%)",
+          zIndex: 10,
+          height: "300px",
+        }}
+      >
+        <ChannelCard channelDetail={channelDetail} />
+      </div>
+    </Box>
+          <Box sx={{
+            mr:{sm: '100px'}
+          }}>
+            <Videos videos={videos} />
+
+          </Box>
+
+    </Box>
+  );
+};
+
+export default ChannelDetail;
--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,41 +1,41 @@
-import {useState, useEffect } from 'react'
-import {Box, Stack, Typography} from '@mui/material'
-import  {Sidebar, Videos}  from '../components'
-import { fetchFromApi } from '../utils/fetchFromApi'
+import React, { useEffect, useState } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+
+import { fetchFromApi } from "../utils/fetchFromApi";
+import { Videos, Sidebar } from "./";
 
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState([]);
 
-  const [selectedCategory, setSelectedCategory] = useState('New')
-  const [videos, setVideos] = useState([])
-
-useEffect(  () => {
- const fetch = async() => {
-  const response = await fetchFromApi(`search?part=snippet&q=${selectedCategory}`)
-  setVideos(response)
+  useEffect(() => {
+    
+const fetchVideos = async () => {
+  const data = await fetchFromApi(`search?part=snippet&q=${selectedCategory}`)
+setVideos(data.items)
  }
-  fetch()
-},[selectedCategory]);
-
+ fetchVideos()
+    }, [selectedCategory]);
 
   return (
-    <Stack sx={{flexDirection: {sx: "column", md: "row"}}}>
-      <Box sx={{ height:{sx: "auto", md:'92vh' }, borderRight: "1px solid #3d3d3d", px: { sx:0, md: 2} }} >
-      <Sidebar 
-      selectedCategory={selectedCategory}
-      setSelectedCategory={setSelectedCategory}
-       />
-        <Typography className='copyright' variant='body2' sx={{ mt:1.5, color: '#fff'}} >
-          Copyright 2023 E Media
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
+        <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
+        
+        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
+          Copyright © 2022 JSM Media
         </Typography>
       </Box>
-      <Box p={2} sx = {{overflowY: 'auto' , height: '90vh' , flex: 2 }}>
-        <Typography variant="h4" font-fontweight="bold" mb={2} sx={{color: 'white'}} >
-         {selectedCategory} <span style={{color: '#F31503'}} >videos</span>
+
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+          {selectedCategory} <span style={{ color: "#FC1503" }}>videos</span>
         </Typography>
+
         <Videos videos={videos} />
       </Box>
     </Stack>
-  )
-}
+  );
+};
 
-export default Feed
+export default Feed;
