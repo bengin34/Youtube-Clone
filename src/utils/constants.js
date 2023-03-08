e26ed57880348f6db1905290c5d5a82064484399
bengin34 <becaglar3434@gmail.com>
2023-03-08 19:32:10 +0300

    name

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -16,9 +16,9 @@ export const logo = "https://i.ibb.co/s9Qys2j/logo.png";
 
 export const categories = [
   { name: "New", icon: <HomeIcon /> },
-  { name: "ReactJS", icon: <CodeIcon /> },
+  { name: "JS Mastery", icon: <CodeIcon /> },
   { name: "Coding", icon: <CodeIcon /> },
-  { name: "JavaScript", icon: <CodeIcon /> },
+  { name: "ReactJS", icon: <CodeIcon /> },
   { name: "NextJS", icon: <CodeIcon /> },
   { name: "Music", icon: <MusicNoteIcon /> },
   { name: "Education", icon: <SchoolIcon /> },
